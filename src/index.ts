3ec39999a0173dc16592e3503449cb282c88f67f
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2023-01-16 13:33:28 +0000

    Add exported `Schemas` class for declaring Zod schemas

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 export { Inngest } from "./components/Inngest";
 export { InngestCommHandler } from "./components/InngestCommHandler";
 export type { ServeHandler } from "./components/InngestCommHandler";
+export { Schemas } from "./components/Schemas";
 export type {
   ClientOptions,
   EventPayload,
   EventPayloads,
-  EventSchemas,
   FunctionOptions,
   InferZodOrObject,
   RegisterOptions,
